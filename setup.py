92454323d6295186ba7c72e8fe769cc078325025
ArchAngel776 <karol.marciniak155@gmail.com>
2022-02-14 02:34:20 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='fcm_interface',
     packages=find_packages(include=['fcm_interface']),
     version='0.1.0',
     description='FCM Interface',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ArchAngel776/fcm_interface",
     license='MIT'
 )
